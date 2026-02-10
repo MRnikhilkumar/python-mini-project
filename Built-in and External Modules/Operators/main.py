a993c5bd3b25e30feb688d83b43253784b254ccf
Nikhil Kumar Singh <sanjeetkumarsinghn@gmail.com>
2026-02-11 00:04:10 +0530

    Add files via upload

---
+++ Built-in and External Modules/Operators/main.py
@@ -0,0 +1,34 @@
+a = 10
+b = 3
+
+# arithmatic operators
+print(a + b)  # addition
+print(a - b)  # substraction
+print(a * b)  # multiplication
+print(a / b)  # division
+print(a // b) # floor division
+print(a ** b) # exponential operator 
+print(a % b)  # modulous
+
+# comparison operators
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a > b)
+print(a <= b)
+print(a < b)
+
+# Assignment Operator
+a += 4
+# a *= 4
+# a -= 4
+# a **= 4 
+print(a)
+
+
+print(True and False)
+print(True and True )
+print(True or False)
+print(True or True )
+print(not(False))
+print(not(True))
---
+++ Built-in and External Modules/f-Strings/main.py
@@ -0,0 +1,7 @@
+name=input("what is your name:" )
+live=input("whare are you live:" )
+work=input("whare do you work:" )
+
+# print(name + " live in "+live +" and work at " + work )  #👎🏽wrong way
+
+print(f"{name} live in {live} and work in  {work}")
